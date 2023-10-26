af706fd657f8482171d500f4d8d97a691ff56619
heejoo <heejoo_byun@naver.com>
2023-10-27 04:26:24 +0900

    Feat [#1] Search Bar 검색했을 때 동작 구현

--- DO_SOPT_Seminar_Assignment/Presentation/SecondSeminar/Main/WeatherStackView.swift
+++ DO_SOPT_Seminar_Assignment/Presentation/SecondSeminar/Main/WeatherStackView.swift
@@ -29,8 +29,7 @@ final class WeatherStackView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setView() {
-
+    func setView() {
         var placeViewTopInset = 0
         
         for i in 0..<7 {
@@ -126,8 +125,100 @@ final class WeatherStackView: UIView {
             
             placeViewTopInset += 133
         }
-        
+    }
+    
+    func showOnlyOneView(_ index: Int) {
+        self.subviews.forEach { $0.removeFromSuperview() }
 
+        let placeView = UIView()
+        let placeViewBackGround = UIButton()
+        let myPlaceLabel = UILabel()
+        let placeLabel = UILabel()
+        let weatherLabel = UILabel()
+        let temperatureLabel = UILabel()
+        let highLowTemperatureLabel = UILabel()
+        
+        placeViewBackGround.do {
+            $0.setImage(ImageLiterals.imgBackGround.imgQuarterBackground, for: .normal)
+            $0.contentVerticalAlignment = .fill
+            $0.contentHorizontalAlignment = .fill
+            $0.addTarget(self, action: #selector(backGroundTapped), for: .touchUpInside)
+        }
+        
+        myPlaceLabel.do {
+            $0.text = StringLiterals.information.myPlace
+            $0.textColor = .white
+            $0.font = .displayBold(ofSize: 24)
+        }
+        
+        placeLabel.do {
+            $0.text = dummy[index].place
+            $0.textColor = .white
+            $0.font = .displayMedium(ofSize: 17)
+        }
+        
+        weatherLabel.do {
+            $0.text = dummy[index].weather
+            $0.textColor = .white
+            $0.font = .displayMedium(ofSize: 16)
+        }
+        
+        temperatureLabel.do {
+            $0.text = dummy[index].temperature
+            $0.textColor = .white
+            $0.font = .displayLight(ofSize: 52)
+        }
+        
+        highLowTemperatureLabel.do {
+            $0.text = dummy[index].highLowTemperature
+            $0.textColor = .white
+            $0.font = .displayMedium(ofSize: 15)
+        }
+        
+        addSubview(placeView)
+        
+        placeView.addSubviews(placeViewBackGround,
+                              myPlaceLabel,
+                              placeLabel,
+                              weatherLabel,
+                              temperatureLabel,
+                              highLowTemperatureLabel)
+        
+        placeView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(0)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(117)
+        }
+
+        placeViewBackGround.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        myPlaceLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10.adjusted)
+            $0.leading.equalToSuperview().inset(16.adjusted)
+        }
+        
+        placeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(44.adjusted)
+            $0.leading.equalToSuperview().inset(16.adjusted)
+        }
+        
+        weatherLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(10.adjusted)
+            $0.leading.equalToSuperview().inset(16.adjusted)
+        }
+        
+        temperatureLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(4.adjusted)
+            $0.trailing.equalToSuperview().inset(16.adjusted)
+        }
+        
+        highLowTemperatureLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16.adjusted)
+            $0.bottom.equalToSuperview().inset(10.adjusted)
+        }
+        
     }
     
     @objc func
--- DO_SOPT_Seminar_Assignment/Presentation/SecondSeminar/Main/WeatherViewController.swift
+++ DO_SOPT_Seminar_Assignment/Presentation/SecondSeminar/Main/WeatherViewController.swift
@@ -11,13 +11,15 @@ import SnapKit
 import Then
 
 final class WeatherViewController: UIViewController {
-
+    
+    private let dummy = Weather.dummy()
+    private var realPageIndex = -1
     private let myScrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: 0, height: 1700))
-
+    
     private let moreButton = UIButton()
     private let titleLabel = UILabel()
     private let searchBar = UISearchBar()
-
+    
     private let myView = WeatherStackView()
     
     override func viewDidLoad() {
@@ -26,7 +28,7 @@ final class WeatherViewController: UIViewController {
         setStyle()
         setLayout()
     }
-
+    
     private func setStyle() {
         view.backgroundColor = .black
         self.navigationController?.setNavigationBarHidden(true, animated: true)
@@ -52,6 +54,7 @@ final class WeatherViewController: UIViewController {
             $0.backgroundColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
             $0.backgroundImage = UIImage()
             $0.makeCornerRound(radius: 10)
+            $0.delegate = self
             $0.setImage(ImageLiterals.icon.icSearch, for: UISearchBar.Icon.search, state: .normal)
             if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
                 textfield.backgroundColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
@@ -75,9 +78,9 @@ final class WeatherViewController: UIViewController {
         view.addSubview(myScrollView)
         
         myScrollView.addSubviews(moreButton,
-                                titleLabel,
-                                searchBar,
-                                myView)
+                                 titleLabel,
+                                 searchBar,
+                                 myView)
         
         myScrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -114,8 +117,44 @@ final class WeatherViewController: UIViewController {
 extension WeatherViewController: BackGroundButtonAction {
     func backGroundTapped(index: Int) {
         let viewController = PageNavigationController()
-        viewController.pageIndex = index
+        if realPageIndex != -1 {
+            viewController.pageIndex = realPageIndex
+        } else {
+            viewController.pageIndex = index
+        }
         self.navigationController?.isNavigationBarHidden = true
         self.navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+extension WeatherViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchBar.text {
+            if let index = dummy.firstIndex(where: {
+                $0.place.contains(searchText) }) {
+                myView.removeFromSuperview()
+                myView.showOnlyOneView(index)
+                realPageIndex = index
+            }
+            myScrollView.addSubview(myView)
+            myView.snp.makeConstraints {
+                $0.top.equalToSuperview().inset(169.adjusted)
+                $0.leading.trailing.equalToSuperview().inset(20.adjusted)
+            }
+        }
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            myView.setView()
+            myScrollView.addSubview(myView)
+            myView.snp.makeConstraints {
+                $0.top.equalToSuperview().inset(169.adjusted)
+                $0.leading.trailing.equalToSuperview().inset(20.adjusted)
+                $0.height.equalTo(950.adjusted)
+                $0.bottom.equalToSuperview()
+            }
+        }
     }
 }
