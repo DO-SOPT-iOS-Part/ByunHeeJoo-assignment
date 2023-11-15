3184630060633951206f6d2ed50b493150ee680f
heejoo <heejoo_byun@naver.com>
2023-11-15 21:02:58 +0900

    Chore [#5] Detail Dummy 수정

--- DO_SOPT_Seminar_Assignment/Presentation/Dummy/WeatherDetailOfWeek.swift
+++ DO_SOPT_Seminar_Assignment/Presentation/Dummy/WeatherDetailOfWeek.swift
@@ -17,16 +17,16 @@ struct WeatherDetailOfWeek {
 
 extension WeatherDetailOfWeek {
     static func dummy() -> [WeatherDetailOfWeek] {
-        return [WeatherDetailOfWeek(day: "오늘", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "15°", highTemperature: "29°"),
-                WeatherDetailOfWeek(day: "월", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "60%", lowTemperature: "18°", highTemperature: "27°"),
-                WeatherDetailOfWeek(day: "화", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "60%", lowTemperature: "20°", highTemperature: "25°"),
-                WeatherDetailOfWeek(day: "수", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "70%", lowTemperature: "17°", highTemperature: "25°"),
-                WeatherDetailOfWeek(day: "목", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "17°", highTemperature: "25°"),
-                WeatherDetailOfWeek(day: "금", weather: ImageLiterals.icon.icSunCloud, rainyPercent: "", lowTemperature: "20°", highTemperature: "26°"),
-                WeatherDetailOfWeek(day: "토", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "18°", highTemperature: "25°"),
-                WeatherDetailOfWeek(day: "일", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "13°", highTemperature: "21°"),
-                WeatherDetailOfWeek(day: "월", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "12°", highTemperature: "19°"),
-                WeatherDetailOfWeek(day: "화", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "18°", highTemperature: "25°"),
-                WeatherDetailOfWeek(day: "수", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "18°", highTemperature: "25°")]}
+        return [WeatherDetailOfWeek(day: "오늘", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "1°", highTemperature: "15°"),
+                WeatherDetailOfWeek(day: "월", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "60%", lowTemperature: "3°", highTemperature: "10°"),
+                WeatherDetailOfWeek(day: "화", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "60%", lowTemperature: "3°", highTemperature: "11°"),
+                WeatherDetailOfWeek(day: "수", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "70%", lowTemperature: "4°", highTemperature: "9°"),
+                WeatherDetailOfWeek(day: "목", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "2°", highTemperature: "8°"),
+                WeatherDetailOfWeek(day: "금", weather: ImageLiterals.icon.icSunCloud, rainyPercent: "", lowTemperature: "2°", highTemperature: "11°"),
+                WeatherDetailOfWeek(day: "토", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "-1°", highTemperature: "7°"),
+                WeatherDetailOfWeek(day: "일", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "0°", highTemperature: "13°"),
+                WeatherDetailOfWeek(day: "월", weather: ImageLiterals.icon.icLittleRain, rainyPercent: "50%", lowTemperature: "4°", highTemperature: "7°"),
+                WeatherDetailOfWeek(day: "화", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "-1°", highTemperature: "5°"),
+                WeatherDetailOfWeek(day: "수", weather: ImageLiterals.icon.icSun, rainyPercent: "", lowTemperature: "-3°", highTemperature: "8°")]}
 }
 
